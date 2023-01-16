1373699ab12ebc9b76565683ac62285a5d9ca1fc
wlad184 <evel184@yandex.ru>
2023-01-16 11:12:59 +0700

    ООП Введение

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        Car myCar1 = new Car("Lada","Granta", 1.7, "", 2015, "Россия" );
+        Car myCar2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "чёрный", 2020, "Германия");
+        Car myCar3 = new Car("BMW", "Z8", 0, "чёрный", 2021, "Германия");
+        Car myCar4 = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2021, "Южная Корея");
+        Car myCar5 = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+
+        System.out.println(myCar1);
+        System.out.println(myCar2);
+        System.out.println(myCar3);
+        System.out.println(myCar4);
+        System.out.println(myCar5);
 
     }
 }
